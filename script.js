2f2c8ba9beb3e90657c85452ace7ec5e60e3be7a
Daiane Silva <daianemsilva09@gmail.com>
2021-07-06 16:42:52 -0300

    Exercicio Fizzbuzz

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-var myName = "Dai";
+//FizzBuzz
+//divisivel por 3 =>'fizz'
+//divisivel por 5 =>'buzz'
+//divisivel por 3 e 5 =>'fizzbuzz'
+// se não for um numero => 'não é um número'
+// se não for divisivel nem por 3 e nem por 5 => Entrada
 
-let languagem = "Javascript";
+let resultado = fizzBuzz(7);
+console.log(resultado)
 
-const teste ="teste"
+function fizzBuzz(entrada) {
+    if (typeof entrada !=='number')
+        return 'Não é um número';
+    if((entrada % 3 === 0) && (entrada % 5 === 0))
+        return 'FizzBuzz';
+    if(entrada % 3 === 0)
+        return 'Fizz';
+    if(entrada % 5 === 0)
+        return 'Buzz';
 
-console.log(myName)
+    return entrada;
 
+}
